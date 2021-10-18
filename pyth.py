8c7406bf6f59b62c4168e02e248ad9bd744670f2
Diego De Paz <diego6632192331@gmail.com>
2021-10-18 01:26:01 -0600

    agregado el tercer archivo .py al proyecto

---
+++ pyth.py
@@ -0,0 +1,15 @@
+#comienzo del proyecto
+
+x = input('hola mundo')
+
+if x:
+    print('como te va')
+else:
+    print('pues no, no te queria hablar')
+
+for i in range(len(x)):
+    if i == 43:
+        break
+    print('hola mundo ', i)
+else:
+    print('todo termino bien')
