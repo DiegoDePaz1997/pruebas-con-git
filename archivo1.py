d1e908b68b6311c54761d72fd601eec1674928e0
Diego De Paz <diego6632192331@gmail.com>
2021-10-18 01:58:58 -0600

    segundo comit que va a github

--- archivo1.py
+++ archivo1.py
@@ -4,3 +4,7 @@ for i in range(10):
 
 texto = 'esto es un texto'
 tex = texto.split().count('esto')
+
+
+for id,i in enumerate(texto):
+    print(f'indice: {id} y la palabra: {i}')
--- otraCosa.py
+++ otraCosa.py
@@ -5,3 +5,7 @@ print(r.random())
 x = [r.random for i in range(10)]
 print(x)
 p.pprint(x)
+
+p.pprint('otras cosas')
+
+print('que solo ocupan espacio')
