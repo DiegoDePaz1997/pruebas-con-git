f51ae92e6824f37a8db497ab5e682c3685964085
Diego De Paz <diego6632192331@gmail.com>
2021-10-19 00:15:49 -0600

    cambios que solo van a esta solo en master

--- otraCosa.py
+++ otraCosa.py
@@ -15,3 +15,7 @@ print('que solo ocupan espacio')
 x = [i**2 for i in range(10) if i%2 ==0]
 print(x)
 print(' '.join(x))
+
+
+print('esto va a estar en master')
+print('esto no va a esta en python_generador')
